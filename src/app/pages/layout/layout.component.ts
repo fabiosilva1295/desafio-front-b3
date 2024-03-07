b72367e0d8ecb92189c9f7509b4464f0111e6a25
fabiosilva1295 <fancis.fabiosilva@gmail.com>
2024-03-07 15:56:35 -0300

    Configuração e Estilização de layouts A e B [web]

---
+++ src/app/pages/layout/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { LayoutType } from 'src/app/models/layout-types.enum';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  defaultLayout: number | string = LayoutType['B3B']
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
