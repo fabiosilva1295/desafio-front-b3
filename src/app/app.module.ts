3366485140fb8d571098584bc50738c5d387d10f
fabiosilva1295 <fancis.fabiosilva@gmail.com>
2024-03-07 15:54:13 -0300

    BROWSERANIMATIONSMODULE, ROUTER MODULE e Criação de components [ Layout, Sidenav, Header, Footer ]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    SidenavComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
