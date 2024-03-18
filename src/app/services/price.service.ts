9a82074d69299670e0ba33da6e3c1d9bdb272b1c
fabiosilva1295 <fancis.fabiosilva@gmail.com>
2024-03-18 16:30:35 -0300

    hotfix

--- src/app/services/price.service.ts
+++ src/app/services/price.service.ts
@@ -18,6 +18,6 @@ export class PriceService {
 
   loadQuotes(from: string = 'USD', to: string = 'BRL' ) {
 
-      this.http.get<any>(`https://api.invertexto.com/v1/currency/${from}_${to}?token=6939|LWHpBFAzfq1tiZD283eV1fuq0Flj0Lef`).subscribe( res => this._currencyQuotes.next(res))
+      this.http.get<any>(`https://api.invertexto.com/v1/currency/${from}_${to}?token=7037|W4HKYBuefsR6OYc61DDTKuzS8vQJ04tw`).subscribe( res => this._currencyQuotes.next(res))
   }
 }
